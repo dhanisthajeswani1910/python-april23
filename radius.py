bfae8877f799ad347636676bd377ca0df9223b2e
dhanisthajeswani1910 <128629979+dhanisthajeswani1910@users.noreply.github.com>
2023-04-30 12:06:58 +0530

    Add files via upload

---
+++ radius.py
@@ -0,0 +1,9 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Accept radius from user
+... radius = float(input("Input the radius of the circle: "))
+... 
+... # Compute area of the circle
+... area = 3.14 * radius * radius
+... 
+... # Print the area
